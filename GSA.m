89a5d2d6b9dffd32d7ba34a8466cff63277243d4
abc <1370197460@qq.com>
2016-03-03 19:04:41 +0800

    Added files via upload

---
+++ GSA.m
@@ -0,0 +1,37 @@
+clc;clear all;close all
+% % ===================================================================== %
+% % A Gravitational Search Algorithm (demo)                               %
+% % ===================================================================== %
+% % Author: aoyuan                                                        %
+% % Release Date: November 18, 2015.                                       %
+% % Modified Date:                                                        %
+% % ===================================================================== %
+% % ===================================================================== %
+help GSA.m
+path = pwd;
+path = path(1:end);
+
+%% Problem Definition
+funcNum =1;
+[Lb, Ub, dim] = funcRange(funcNum);  % for objFunc: twenty-three benchmark functions   
+
+%% GSA Parameters
+para = struct;
+para.NGen=10000;        % Maximum Number of Iterations
+para.NP = 100;          % Population Size
+para.G0 = 100;         % GSA G0
+para.alfa = 20;        % GSA alfa
+para.Rpower = 1;       % GSA power of 'R'
+para.ElitistCheck=1; 
+
+%% Main function
+Nr = 1;
+for run = 1:Nr
+    rand('state',sum(100*clock))
+    tic;
+    BestChart = GSA_func('objFunc',para,Lb, Ub, dim,funcNum);       
+    t = toc;
+    fprintf('R = %2d, time = %3.5f, error = %10.3e\n', run, t, BestChart(end))        
+end 
+
+
